7f464cf4ffd756b667c697966be8c7484371f642
hija0016 <france@DESKTOP-K3IQUM1.localdomain>
2019-04-17 15:10:01 +0800

    swiper

--- src/App.js
+++ src/App.js
@@ -1,39 +1,71 @@
 import React, { Component } from 'react';
-import { Image } from 'react-native';
-import { Container, Header, View, DeckSwiper, Card, CardItem, Thumbnail, Text, Left, Body, Icon } from 'native-base';
-import { StatusBar } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Image
+} from 'react-native';
+  
+  import Swiper from 'react-native-swiper';
+  import { StatusBar } from 'react-native';
 
-
-export default class App extends Component {
-  componentDidMount() {
-    StatusBar.setHidden(true);
-  }
-  componentWillMount() {
-    setTimeout(() => {
-       this.props.navigation.navigate('Screen1')
-    }, 3000);
-}
-
-  render() {
-    return (
-
-        <View style={styles.container}>
+  export default class App extends Component {
+    componentDidMount() {
+      StatusBar.setHidden(true);
+    }
+    render(){
+      return (
+        <Swiper style={styles.wrapper} paginationStyle={{position:'absolute', bottom: -50}} showsButtons={false}>
+            <View style={styles.container}>
             <Image
             style={{width: '100%', height: '100%'}}
-            source={require('./Animation/splash.gif')}
+            source={require('./Animation/scene1+2.gif')}
             />
 
-       </View>
-    );
+            </View>
+          <View style={styles.slide2}>
+            <Text style={styles.text}>Beautiful</Text>
+          </View>
+          <View style={styles.slide3}>
+            <Text style={styles.text}>And simple</Text>
+          </View>
+        </Swiper>
+      );
+    }
   }
-}
-
-
-const styles = {
+  
+  const styles = StyleSheet.create({
+    wrapper: {
+    },
+    slide1: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: 'red',
+    },
+    slide2: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#97CAE5',
+    },
+    slide3: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#92BB',
+    },
+    text: {
+      color: '#fff',
+      fontSize: 30,
+      fontWeight: 'bold',
+    },
     container: {
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: '#ecf0f1',
     },
-  };
+  })
+
+  
