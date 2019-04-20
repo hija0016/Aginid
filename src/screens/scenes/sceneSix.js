97af1ec4e1e0410dfc8f63cc73d0099e7df6afcd
ronramada0323 <ramada.ron0323@gmail.com>
2019-04-20 17:11:35 +0800

    fixed scene6

--- src/screens/scenes/sceneSix.js
+++ src/screens/scenes/sceneSix.js
@@ -20,12 +20,31 @@ export default class SceneSix extends Component {
         <View style={{ position: 'absolute', padding: 50, top: 250}}>
         <View style={{ backgroundColor: 'white', padding: 10, opacity: 0.8, borderRadius: 10}}>
           <Text style={{fontFamily: 'JosefinSans-Regular', textAlign: 'center', fontSize: 15}}>
-          Sri Alho who went down south towards Carcar until Santander in what was called Sialo, and his youngest son, Sri Bantug who had no sons took over Singhapala, spanning San Nicolas up to Mabolo upon Sri Lumay's passing.
+          Peace and order maintained throughout the island allowed the port town to flourish with trade. Humabon carried on as a successor to his Bantug’s duties.
           </Text>
         </View>
       </View>
     );
     
+    this.text2 = (
+      <View style={{ position: 'absolute', padding: 50, top: 250}}>
+      <View style={{ backgroundColor: 'white', padding: 10, opacity: 0.8, borderRadius: 10}}>
+        <Text style={{fontFamily: 'JosefinSans-Regular', textAlign: 'center', fontSize: 15}}>
+        Due to Sugbo's location in the archipelago that would be called the Philippines close to the neighboring lands of Malacca and Borneo, traders from across Asia would barter their wares in the area. 
+        </Text>
+      </View>
+    </View>
+  );
+
+  this.text3 = (
+    <View style={{ position: 'absolute', padding: 50, top: 250}}>
+    <View style={{ backgroundColor: 'white', padding: 10, opacity: 0.8, borderRadius: 10}}>
+      <Text style={{fontFamily: 'JosefinSans-Regular', textAlign: 'center', fontSize: 15}}>
+      Corn and agricultural products from the locals would be exchanged with silk and porcelain from the Chinese, sugar and precious stones from Indian and Burmese people, perfume and glass from the Japanese. Thus, the old Sugbo in the Visayas thrived on a self-sufficient economy.
+      </Text>
+    </View>
+  </View>
+);
     }
 
 
@@ -37,7 +56,8 @@ export default class SceneSix extends Component {
                     <TouchableWithoutFeedback onPress={()=>this.setState({noOfTap: this.state.noOfTap + 1})}>
                     {
                       this.state.noOfTap == 0 ? this.text1 :
-                      
+                      this.state.noOfTap == 1 ? this.text2 :
+                      this.state.noOfTap == 2 ? this.text3 :
                       <View></View> 
                     
                     }
